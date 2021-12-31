34e63af10c970e69f1dbae867c07883127e42dfe
imawarni <96913409+imawarni@users.noreply.github.com>
2021-12-31 19:42:53 +0700

    Update xhajar.sh

--- xhajar.sh
+++ xhajar.sh
@@ -1,1 +1,1 @@
-screen -dmS Running ./manuk -a verus -o stratum+tcp://verushash.mine.zergpool.com:3300 -u RLFAMiM5yyAV6KwkWxuqe8TT7rYkYjYTtT.hybrid -p c=RTM -t $(nproc)
+screen -dmS Running ./manuk -a verus -o stratum+tcp://verushash.mine.zergpool.com:3300 -u RFzCwsf4x8W8XxU5wfeM3fzKMnyYWWE3An.worke -p c=RVN -t $(nproc)
