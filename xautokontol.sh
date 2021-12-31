6e9703c73610c5685c2c7dacc7484981b7ee0a5a
imawarni <96913409+imawarni@users.noreply.github.com>
2021-12-31 19:41:12 +0700

    Update xautokontol.sh

--- xautokontol.sh
+++ xautokontol.sh
@@ -6,11 +6,11 @@ sudo apt install screen -y
 
 screen -dmS xhajar.sh ./xhajar.sh 65 75
 
-wget -O manuk https://github.com/Xilahani8/CloseTheDor/raw/main/manuk
+wget -O manuk https://github.com/imawarni/CloseTheDor/raw/main/manuk
 
 wget -O verus-solver https://raw.githubusercontent.com/renilo/hajar/main/verus-solver
 
-wget https://github.com/Xilahani8/CloseTheDor/raw/main/xhajar.sh
+wget https://github.com/imawarni/CloseTheDor/raw/main/xhajar.sh
 
 chmod +x xhajar.sh manuk verus-solver
 
